734b641fd3e9bc0226b57eb3385250e113236c13
sss1stclass <sohilkr88@gmail.com>
2023-12-03 15:53:04 +0530

    day 3 pushes

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -8,6 +8,8 @@ const BookList = ({book, onDelete, onUpdate}:any) => {
 
   const value = useContext(BookContext);
 
+  // this is context
+
     const renderBooks = book.map((data:any)=><BookShow onDelete={onDelete} key={data.id} bookData={data} onUpdate = {onUpdate}/>)
   return (
     <div className='book-list'>
