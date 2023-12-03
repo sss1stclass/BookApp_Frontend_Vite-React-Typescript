486499dd54867363374a8dbb9c372aaaaceef91b
sss1stclass <sohilkr88@gmail.com>
2023-12-03 15:58:08 +0530

    reverting the code

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -8,7 +8,7 @@ const BookList = ({book, onDelete, onUpdate}:any) => {
 
   const value = useContext(BookContext);
 
-  // this is context
+
 
     const renderBooks = book.map((data:any)=><BookShow onDelete={onDelete} key={data.id} bookData={data} onUpdate = {onUpdate}/>)
   return (
